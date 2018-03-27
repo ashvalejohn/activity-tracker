7d5673109e73417fa58b1bcf6d063de4dbba27be
Ashley Johnson <ashvalejohn@gmail.com>
2018-03-27 12:57:04 -0700

    Remove create-react-app defaults

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/nav';
 import CheckIn from './components/check-in';
 import Report from './components/report';
