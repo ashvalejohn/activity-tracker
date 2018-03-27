b04d46865a8b562986e77e7ba70b025933e8a1de
Ashley Johnson <ashvalejohn@gmail.com>
2018-03-26 21:55:31 -0700

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Nav from './components/nav';
+import CheckIn from './components/check-in';
+import Report from './components/report';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router className="App">
+        <div>
+          <Route path='/' component={Nav} />
+          <Route path='/report' component={Report} />
+          <Route default path='/check-in' component={CheckIn} />
+        </div>
+      </Router>
     );
   }
 }
