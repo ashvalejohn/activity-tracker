e73914ada41f2bf35df8ef88a9928083f1990b04
Ashley Johnson <ashvalejohn@gmail.com>
2018-03-28 17:55:22 -0700

    Display gradient blocks, show activity on hover

---
+++ src/components/block.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Block = (props) => {
+  return (
+    <div className={`block block--${props.feeling > 50 ? 'good' : 'bad'}`}>
+      
+        <p className='block__activity'>{props.activity}</p>
+      
+    </div>
+  );
+}
+
+export default Block;
