c25d48575e86daadb99937fd9e7eceea84529e6e
Ashley Johnson <ashvalejohn@gmail.com>
2018-03-28 17:47:35 -0700

    Iterate over reports to display feelings grid

--- src/components/report.jsx
+++ src/components/report.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import Block from './block';
+import { reports } from '../reports.js';
 
 const Report = () => {
+  console.log(reports);
   return (
-    <form>
-      <label>
-        Here’s how you felt about your day:
-        <div>
-          INSERT GRAPH
-        </div>
-      </label>
-    </form>
+    <div>
+      <h2>Here’s how you felt about your day:</h2>
+      <div>
+        {
+          reports.map((report, index) => {
+            return <Block activity={report.activity} feeling={report.feeling} key={index}/>
+          })
+        }
+      </div>
+    </div>
   );
 }
 
