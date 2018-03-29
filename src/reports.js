40a585c14b5f3d1b9dc0c4316a327e739875add1
Ashley Johnson <ashvalejohn@gmail.com>
2018-03-28 17:49:01 -0700

    Dummy report data

---
+++ src/reports.js
@@ -0,0 +1,14 @@
+export const reports = [
+  {
+    "activity": "Danced to Pretty Young Thing.",
+    "feeling": "34"
+  },
+  {
+    "activity": "Completed final research paper!",
+    "feeling": "100"
+  },
+  {
+    "activity": "Watched late night interviews with Bill Hader.",
+    "feeling": "4"
+  }
+]
