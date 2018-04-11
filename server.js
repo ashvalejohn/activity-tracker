f1714f9b60bb2378fe82cb19bb19e142d8f1af7e
Ashley Johnson <ashvalejohn@gmail.com>
2018-04-11 08:27:16 -0700

    Add Mongoose, initial schema and model

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000;
+const express = require("express"),
+      app = express(),
+      mongoose = require("mongoose"),
+      port = process.env.PORT || 5000;
+
+mongoose.connect("mongodb://localhost/activities");
+
+// Schema Setup
+const reportSchema = new mongoose.Schema({
+  activity: String,
+  feeling: String,
+  date: {type: Date, default: Date.now },
+});
+
+const Report = mongoose.model("Report", reportSchema);
+
+Report.create({
+  activity: "Learning Mongoose",
+  feeling: "80",
+});
 
 app.set("view engine", "ejs");
 
